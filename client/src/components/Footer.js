3da7e11f9ae6e511e9c066694e11b9dcff63e069
Christian Martinez <christian@mixdlabs.com>
2023-09-23 13:06:35 -0600

    Copyright text in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-zinc-50 py-4 pt-20">
       <div className="relative z-0 mx-auto flex w-full items-center justify-center p-4">
         <span className="text-md text-zinc-400 sm:text-center">
-          <h1>&lt;codeathon&sol;&gt; {new Date().getFullYear()}</h1>
+          &copy; Codethon {new Date().getFullYear()}
         </span>
       </div>
     </footer>
