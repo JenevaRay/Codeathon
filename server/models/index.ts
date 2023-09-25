79f65cf4a160ebfee96b54b039214e637c2249cf
Adam Hansen <adamhansen@me.com>
2023-09-24 18:50:04 -0600

    Update ERD and bump schema version to 0.0.3

--- server/models/index.ts
+++ server/models/index.ts
@@ -8,7 +8,7 @@ import { Event } from './Event'
 import { Group } from './Group'
 
 // Define a schema version and a schema date for future compatibility
-const schemaVersion = '0.0.2';
+const schemaVersion = '0.0.3';
 const schemaDate = dayjs('2023-09-24');
 
 // Export all models plus the schema version and schema date
