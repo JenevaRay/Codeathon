eb7e8974b4e1cb33feaf4f8e6b7a56fece648aa4
Adam Hansen <adamhansen@me.com>
2023-09-25 19:25:53 -0600

    Seed dynamic dates and times
    based upon the current date and time using Day.js

--- server/config/seeds.ts
+++ server/config/seeds.ts
@@ -1,17 +1,17 @@
-// TODO: import dayjs from 'dayjs'; for working with dates
+import dayjs from 'dayjs';
 // import mongoose from 'mongoose'
 // const ObjectID = mongoose.Types.ObjectId
 
 import { db } from './connection';
 
-import { User, Event, schemaVersion, schemaDate, Group, Venue, Registration, Address, Phone } from '../models/index';
+import { User, Event, schemaVersion, schemaDate, Group, Venue, Registration, Address, Phone } from '../models/';
 
-// TODO: Switch to dayjs objects to seed dynamic dates based upon the current date
-const now: Date = new Date("September 24, 2023")
-const lastMonth: Date = new Date("August 24, 2023")
-const weekAgo: Date = new Date("September 17, 2023")
-const nextWeek: Date = new Date("October 1, 2023")
-const nextMonth: Date = new Date("October 24, 2023")
+// Seed dynamic dates and times based upon the current date and time using Day.js
+const now = dayjs();
+const lastMonth = now.subtract(1, 'month');
+const weekAgo = now.subtract(1, 'week');
+const nextWeek = now.add(1, 'week');
+const nextMonth = now.add(1, 'month');
 
 const events = [
   {
@@ -19,11 +19,11 @@ const events = [
     schemaVersion: schemaVersion,
     schemaDate: schemaDate.toDate(),
     name: "EventNowNoRegistrations",
-    dateStart: weekAgo,
-    dateEnd: nextWeek,
+    dateStart: weekAgo.toDate(),
+    dateEnd: nextWeek.toDate(),
     registrations: [],
-    registrationCutoffDate: now,
-    registrationPaymentRequiredDate: now,
+    registrationCutoffDate: now.toDate(),
+    registrationPaymentRequiredDate: now.toDate(),
     organizerUserId: '000000111111',
     feeRegistration: 1299,
     feeVenue: 1000,
@@ -35,11 +35,11 @@ const events = [
     schemaVersion: schemaVersion,
     schemaDate: schemaDate.toDate(),
     name: "EventExpired",
-    dateStart: lastMonth,
-    dateEnd: lastMonth,
+    dateStart: lastMonth.toDate(),
+    dateEnd: lastMonth.toDate(),
     registrations: ['012345012345', '987654987654'],
-    registrationCutoffDate: lastMonth,
-    registrationPaymentRequiredDate: lastMonth,
+    registrationCutoffDate: lastMonth.toDate(),
+    registrationPaymentRequiredDate: lastMonth.toDate(),
     organizerUserId: '000000111111',
     feeRegistration: 1299,
     feeVenue: 1000,
@@ -51,11 +51,11 @@ const events = [
     schemaVersion: schemaVersion,
     schemaDate: schemaDate.toDate(),
     name: "EventFutureAcceptingRegistrations",
-    dateStart: nextMonth,
-    dateEnd: nextMonth,
+    dateStart: nextMonth.toDate(),
+    dateEnd: nextMonth.toDate(),
     registrations: [],
-    registrationCutoffDate: nextWeek,
-    registrationPaymentRequiredDate: nextMonth,
+    registrationCutoffDate: nextWeek.toDate(),
+    registrationPaymentRequiredDate: nextMonth.toDate(),
     organizerUserId: '000000111111',
     feeRegistration: 1399,
     feeVenue: 1000,
@@ -65,87 +65,88 @@ const events = [
 ]
 
 const users = [
-    {
-        _id: '000000111111',
-        schemaVersion: schemaVersion,
-        schemaDate: schemaDate.toDate(),
-        nameFirst: "Dave",
-        nameLast: "Smith",
-        email: "davesmith@acme.net",
-        emailType: 'work',
-        phoneNumbers: ['111112222233'],
-        registrations: ['012345012345'],
-        otherContactMethod: null,
-        preferredContactMethod: "email",
-        password: "=^^=NoSuchPassword:3",
-        addresses: ['224466224466']
-    },
-    {
-        _id: '222222333333',
-        schemaVersion: schemaVersion,
-        schemaDate: schemaDate.toDate(),
-        registrations: ['987654987654'],
-        firstName: "First",
-        lastName: "Last",
-        email: "lastfirst@lifo.org",
-        emailType: 'personal',
-        phoneNumbers: ['222223333344'],
-        otherContactMethod: null,
-        preferredContactMethod: "email",
-        password: "itsonthefridge",
-        addresses: ['224466224466']
-    }
+  {
+    _id: '000000111111',
+    schemaVersion: schemaVersion,
+    schemaDate: schemaDate.toDate(),
+    nameFirst: "Dave",
+    nameLast: "Smith",
+    email: "davesmith@acme.net",
+    emailType: 'work',
+    phoneNumbers: ['111112222233'],
+    registrations: ['012345012345'],
+    otherContactMethod: null,
+    preferredContactMethod: "email",
+    password: "=^^=NoSuchPassword:3",
+    addresses: ['224466224466']
+  },
+  {
+    _id: '222222333333',
+    schemaVersion: schemaVersion,
+    schemaDate: schemaDate.toDate(),
+    registrations: ['987654987654'],
+    firstName: "First",
+    lastName: "Last",
+    email: "lastfirst@lifo.org",
+    emailType: 'personal',
+    phoneNumbers: ['222223333344'],
+    otherContactMethod: null,
+    preferredContactMethod: "email",
+    password: "itsonthefridge",
+    addresses: ['224466224466']
+  }
 ]
 
 const groups = [
-    {
-        _id: '554466554466',
-        schemaVersion: schemaVersion,
-        schemaDate: schemaDate.toDate(),
-        registrations: ['012345012345', '987654987654'],
-        eventId: '999988887777',
-        name: 'Together Now',
-        projectName: 'Full Schema Projector',
-        projectDescription: 'Laid out to see'
-    }
-]  
+  {
+    _id: '554466554466',
+    schemaVersion: schemaVersion,
+    schemaDate: schemaDate.toDate(),
+    registrations: ['012345012345', '987654987654'],
+    eventId: '999988887777',
+    name: 'Together Now',
+    projectName: 'Full Schema Projector',
+    projectDescription: 'Laid out to see'
+  }
+]
 
 const addresses = [
-    {
-        _id: '224466224466',
-        schemaVersion: schemaVersion,
-        schemaDate: schemaDate.toDate(),    
-        streetAddress: "123 Sesame Street",
-        extendedAddress: undefined,
-        country: 'US',
-        state: 'New York',
-        county: 'New York',
-        city: 'Manhattan',
-        postalCode: '12345',
-        type: 'venue',
-        isUserPrimary: false,
-    }
+  {
+    _id: '224466224466',
+    schemaVersion: schemaVersion,
+    schemaDate: schemaDate.toDate(),
+    streetAddress: "123 Sesame Street",
+    extendedAddress: undefined,
+    country: 'US',
+    state: 'New York',
+    county: 'New York',
+    city: 'Manhattan',
+    postalCode: '12345',
+    type: 'venue',
+    isUserPrimary: false,
+  }
 ]
 
 const phones = [
-    {
-        schemaVersion: schemaVersion,
-        schemaDate: schemaDate.toDate(),
-        type: 'venue',    
-        _id: '111112222233',
-        number: "+1 234-567-8901",
-        isUserPrimary: false
-    }, {
-        schemaVersion: schemaVersion,
-        schemaDate: schemaDate.toDate(),
-        type: 'cell',
-        _id: '222223333344',
-        number: "+1 (234) 567-0001",
-        isUserPrimary: true
-    }
+  {
+    schemaVersion: schemaVersion,
+    schemaDate: schemaDate.toDate(),
+    type: 'venue',
+    _id: '111112222233',
+    number: "+1 234-567-8901",
+    isUserPrimary: false
+  }, {
+    schemaVersion: schemaVersion,
+    schemaDate: schemaDate.toDate(),
+    type: 'cell',
+    _id: '222223333344',
+    number: "+1 (234) 567-0001",
+    isUserPrimary: true
+  }
 ]
 
-const venues = [{
+const venues = [
+  {
     _id: '555666555666',
     schemaVersion: schemaVersion,
     schemaDate: schemaDate.toDate(),
@@ -154,29 +155,30 @@ const venues = [{
     phoneId: '111112222233',
     venueTimeZone: 'EST',
     hostId: '000000111111'
-}]
+  }
+]
 
 const registrations = [
-    {
-        _id: '012345012345',
-        schemaVersion: schemaVersion,
-        schemaDate: schemaDate,
-        userId: '000000111111',
-        eventId: '888877776666',
-        registrationDate: lastMonth,
-        registrationType: 'host',
-        // hosts don't pay, or more technically pay $0
-        paid: true
-    }, {
-        _id: '987654987654',
-        schemaVersion,
-        schemaDate,
-        userId: '222222333333',
-        eventId: '888877776666',
-        registrationDate: lastMonth,
-        registrationType: 'attendee',
-        paid: true
-    }
+  {
+    _id: '012345012345',
+    schemaVersion: schemaVersion,
+    schemaDate: schemaDate.toDate(),
+    userId: '000000111111',
+    eventId: '888877776666',
+    registrationDate: lastMonth.toDate(),
+    registrationType: 'host',
+    // hosts don't pay, or more technically pay $0
+    paid: true
+  }, {
+    _id: '987654987654',
+    schemaVersion,
+    schemaDate,
+    userId: '222222333333',
+    eventId: '888877776666',
+    registrationDate: lastMonth,
+    registrationType: 'attendee',
+    paid: true
+  }
 ]
 
 db.once('open', async () => {
